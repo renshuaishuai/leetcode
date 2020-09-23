d0ac33db9f28a82778801f4b5712163552174e0f
shuaishuai.ren <shuaishuai.ren@brgroup.com>
2020-09-23 13:40:27 +0800

    606. 根据二叉树创建字符串

---
+++ src/main/java/com/natsu/leetcode/LeetCode606.java
@@ -0,0 +1,45 @@
+package com.natsu.leetcode;
+
+/**
+ * 606. 根据二叉树创建字符串
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ * <p>
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ *
+ * @author shuaishuai.ren
+ * @since 2020-09-23
+ */
+public class LeetCode606 {
+
+    public static String tree2str(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        if (root.left == null && root.right == null) {
+            return root.val + "";
+        }
+        if (root.right == null) {
+            return root.val + "(" + tree2str(root.left) + ")";
+        }
+        return root.val + "(" + tree2str(root.left) + ")" + "(" + tree2str(root.right) + ")";
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        System.out.println(tree2str(root));
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
